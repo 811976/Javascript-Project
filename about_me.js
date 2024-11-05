6344d8cb78cd2b6c0d85ef96fac0f1be85f5857f
811976 <811976@seq.org>
2024-11-05 07:54:40 -0800

    Hyperlink Buttons for navigating btwn pages. Not yet working

---
+++ about_me.js
@@ -0,0 +1,11 @@
+// switching from About Me to Sports & Hobbies
+var button2 = document.getElementById("button2");
+button2.addEventListener('click', function() {
+    window.location.href = "sports_and_hobbies.html";
+});
+
+// switching from About Me to Skills & Experience
+var button3 = document.getElementById("button3");
+button3.addEventListener('click', function() {
+    window.location.href = "skills_and_experience.html";
+});
