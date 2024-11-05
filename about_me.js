3c202c669ed3ca9d654f994abca69325b964c575
811976 <811976@seq.org>
2024-11-05 09:01:33 -0800

    Update about_me.js
    test full path

--- about_me.js
+++ about_me.js
@@ -1,7 +1,7 @@
 // switching from About Me to Sports & Hobbies
 var button2 = document.getElementById("button2");
 button2.addEventListener('click', function() {
-    window.location.href = "sports_and_hobbies.html";
+    window.location.href = "/Users/jonny/Documents/GitHub/Javascript-Project/sports_and_hobbies.html";
 });
 
 // switching from About Me to Skills & Experience
